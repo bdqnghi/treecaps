775366a1fabcf1201a76929f1f124287da3b9218
Quoc Nghi <bdqnghi@gmail.com>
2020-04-15 23:40:38 +0800

    update

--- main.py
+++ main.py
@@ -324,19 +324,19 @@ def analysis(test_trees, embedding_lookup, opt):
                 f.write(line)
                 f.write("\n")
 
-        for label, class_capsule in enumerate(class_caps_score):
+        # for label, class_capsule in enumerate(class_caps_score):
             
-            line = str(label) + ","
-            # capsule = capsule.tolist()
-            capsule_score = []
-            for score in class_capsule:
-                capsule_score.append(str(score))
+        #     line = str(label) + ","
+        #     # capsule = capsule.tolist()
+        #     capsule_score = []
+        #     for score in class_capsule:
+        #         capsule_score.append(str(score))
 
-            capsule_score = " ".join(capsule_score)
-            with open(class_caps_analysis_path, "a") as f:
-                line = line + capsule_score
-                f.write(line)
-                f.write("\n")
+        #     capsule_score = " ".join(capsule_score)
+        #     with open(class_caps_analysis_path, "a") as f:
+        #         line = line + capsule_score
+        #         f.write(line)
+        #         f.write("\n")
 
 # def test(trees , name):
 #     all_tuples = []
